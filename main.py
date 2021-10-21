75dc4c9509566a75b41cd761110c9abd2a0f3a86
Matheus Mazali Maeda <79715828+Yatogami-Tohka1@users.noreply.github.com>
2021-10-21 19:16:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,67 +2,86 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
 
-participantes = ['Ane', 'Camila']
-
 votoscamila = []
 votosane = []
 
 @client.event
 async def on_ready():
-    print('Iniciado')
+    print('Iniciado como {}'.format(client.user))
 
 @client.command()
 async def bet(ctx, args1):
     if args1.lower() == 'camila':
-        if f'{ctx.message.author.name}' in votosane:
-            votosane.remove(str(ctx.message.author.name))
-            votoscamila.append(str(ctx.author.name))
-            await ctx.message.channel.send(f'`{ctx.message.author.name}` trocou o voto para *Camila*!')
+        if f'<@{str(ctx.message.author.id)}>' in votosane:
+            votosane.remove(f'<@{str(ctx.message.author.id)}>')
+            votoscamila.append(f'<@{str(ctx.message.author.id)}>')
+            await ctx.message.channel.send(f'<@{ctx.message.author.id}> trocou o voto para **Camila**!')
             return
 
-        if f'{ctx.message.author.name}' in votoscamila:
-            await ctx.message.channel.send(f'`{ctx.message.author.name}` já apostou na *Camila*!')
+        if f'<@{str(ctx.message.author.id)}>' in votoscamila:
+            await ctx.message.channel.send(f'<@{ctx.message.author.id}> já apostou na **Camila**!')
             return
 
-        votoscamila.append(str(ctx.author.name))
-        await ctx.message.channel.send(f'`{ctx.message.author.name}` apostou na *Camila* com sucesso!')
+        votoscamila.append(f'<@{str(ctx.message.author.id)}>')
+        await ctx.message.channel.send(f'<@{ctx.message.author.id}> apostou na **Camila** com sucesso!')
 
     if args1.lower() == 'ane':
-        if f'{ctx.message.author.name}' in votoscamila:
-            votoscamila.remove(str(ctx.message.author.name))
-            votosane.append(str(ctx.author.name))
-            await ctx.message.channel.send(f'`{ctx.message.author.name}` trocou o voto para *Ane*!')
+        if f'<@{str(ctx.message.author.id)}>' in votoscamila:
+            votoscamila.remove(f'<@{str(ctx.message.author.id)}>')
+            votosane.append(f'<@{str(ctx.message.author.id)}>')
+            await ctx.message.channel.send(f'<@{ctx.message.author.id}> trocou o voto para **Ane**!')
             return
 
-        if f'{str(ctx.message.author.name)}' in votosane:
-            await ctx.message.channel.send(f'`{ctx.message.author.name}` já apostou na *Ane*!')
+        if f'<@{str(ctx.message.author.id)}>' in votosane:
+            await ctx.message.channel.send(f'<@{ctx.message.author.id}> já apostou na **Ane**!')
             return
 
-        votosane.append(str(ctx.author.name))
-        await ctx.message.channel.send(f'`{ctx.message.author.name}` apostou na *Ane* com sucesso!')
+        votosane.append(f'<@{str(ctx.message.author.id)}>')
+        await ctx.message.channel.send(f'<@{ctx.message.author.id}> apostou na **Ane** com sucesso!')
 
 
 @client.command()
 async def placar(message):
-    await message.channel.send(str('>                      **Participantes**     \n').center(50, ' '))
-    for i in range(0, len(participantes)):
-        await message.channel.send('> ***' + participantes[i] + '*** **:** \n')
-        if i == 1:
-            for c in range(0, len(votoscamila)):
-                await message.channel.send('>          **' + votoscamila[c] + '**')
-        if i == 0:
-            for a in range(0, len(votosane)):
-                await message.channel.send('>          **' + votosane[a] + '**')
+    await message.channel.send(str('>>>                            **Participantes**\n'
+                                   '**Camila : **\n'
+                                   + '\n'.join(votoscamila)) +
+                                   '\n **Ane :** \n'
+                                   + '\n'.join(votosane))
 
 @client.command()
-async def limpar(ctx):
-    if f'{ctx.message.author.name}' in votoscamila:
-        votoscamila.remove(str(ctx.message.author.name))
-        await ctx.message.channel.send(f'`{ctx.message.author.name}` teve o voto removido da participante *Camila*!')
-    if f'{ctx.message.author.name}' in votosane:
-        votosane.remove(str(ctx.message.author.name))
-        await ctx.message.channel.send(f'`{ctx.message.author.name}` teve o voto removido da participante *Ane*!')
-    else:
-        await ctx.message.channel.send(f'`{ctx.message.author.name}` não votou em nenhuma participante ainda!')
+async def retirar(ctx):
+    if f'<@{str(ctx.message.author.id)}>' in votoscamila:
+        votoscamila.remove(f'<@{str(ctx.message.author.id)}>')
+        await ctx.message.channel.send(f'<@{ctx.message.author.id}> teve o voto removido da participante **Camila**!')
+        return
+    if f'<@{str(ctx.message.author.id)}>' in votosane:
+        votosane.remove(f'<@{str(ctx.message.author.id)}>')
+        await ctx.message.channel.send(f'<@{ctx.message.author.id}> teve o voto removido da participante **Ane**!')
+        return
+    await ctx.message.channel.send(f'<@{ctx.message.author.id}> não votou em nenhuma participante ainda!')
 
-client.run('Secret')
+@client.command()
+async def resetar(ctx):
+    await ctx.message.channel.send('Os votos foram resetados!')
+    votoscamila.clear()
+    votosane.clear()
+
+@client.command()
+async def vitoria(ctx, *, args1):
+    if len(votoscamila) == 0 and len(votosane) == 0:
+        await ctx.message.channel.send('Nenhum voto encontrado!')
+        return
+    if args1.lower() == 'camila':
+        await ctx.message.channel.send(str('>>>                            **Vitória da Camila**\n'
+                                       '**Pessoas que acreditaram no potencial dela : **\n'
+                                       + '\n'.join(votoscamila)))
+        votoscamila.clear()
+        votosane.clear()
+    if args1.lower() == 'ane':
+        await ctx.message.channel.send(str('>>>                            **Vitória da Ane**\n'
+                                       '**Pessoas que acreditaram no potencial dela : **\n'
+                                       + '\n'.join(votosane)))
+        votoscamila.clear()
+        votosane.clear()
+
+client.run('SECRET')
